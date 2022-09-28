cd9d244037ddc305e11aa04c7e3ef5e5774fd92a
anastasiya <nastya.panysh@gmail.com>
2022-09-28 19:55:38 +0300

    edit js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getDate() {
     const today = new Date(); // текущая дата
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
+    const hours = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours();
+    const minutes = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes();
     const year = today.getFullYear();
     document.querySelector('.time').innerHTML = hours + ':' + minutes;
     document.querySelector('.year').innerHTML = year;
