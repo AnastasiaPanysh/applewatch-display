42774ec026e716501fc4b9980d90367bb1608e45
anastasiya <nastya.panysh@gmail.com>
2022-10-23 22:46:00 +0300

    add oop style

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-function getDate() {
-    const today = new Date(); // текущая дата
-    const hours = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours();
-    const minutes = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes();
-    const year = today.getFullYear();
-    document.querySelector('.time').innerHTML = hours + ':' + minutes;
-    document.querySelector('.year').innerHTML = year;
+class Watch {
+    getDate() {
+        const today = new Date();
+        const hours = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours();
+        const minutes = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes();
+        const year = today.getFullYear();
+        document.querySelector('.time').innerHTML = hours + ':' + minutes;
+        document.querySelector('.year').innerHTML = year;
+    }
+
 }
-setInterval(getDate, 1000);
+const watch = new Watch()
+console.log(watch.getDate(setInterval, 1000));
